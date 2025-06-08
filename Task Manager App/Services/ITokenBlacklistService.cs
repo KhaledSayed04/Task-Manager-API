6a6be7022c78ce38fb1517a4ee1a5ef3a38bf9f9
KhaledSayed04 <KhaledSA15Me@gmail.com>
2025-06-08 22:19:26 +0300

    Initial Version

---
+++ Task Manager App/Services/ITokenBlacklistService.cs
@@ -0,0 +1,8 @@
+﻿namespace Task_Manager.ApiService.Services
+{
+    public interface ITokenBlacklistService
+    {
+        Task BlacklistTokenAsync(string token, DateTime expiration);
+        Task<bool> IsTokenBlacklistedAsync(string token);
+    }
+}
